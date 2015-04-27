ae053e47b3082bacee59130651449886222d5c48
Michael Hausenblas <michael.hausenblas@gmail.com>
2015-04-27 16:11:59 +0100

    implements countdown timer client-side

--- content/ntil.js
+++ content/ntil.js
@@ -19,6 +19,7 @@ function getTargetDate() {
         $("#countdown").html(d.date);
         console.debug("GET " + apicall);
         console.debug(d);
+        countDownTimer(d.date, 'countdown');
     });
 }
 
@@ -29,4 +30,35 @@ function getTargetTopic() {
         console.debug("GET " + apicall);
         console.debug(d);
     });
+}
+
+// lifted from http://stackoverflow.com/questions/9335140/how-to-countdown-to-a-date
+function countDownTimer(dt, id){
+       var end = new Date(dt);
+       var _second = 1000;
+       var _minute = _second * 60;
+       var _hour = _minute * 60;
+       var _day = _hour * 24;
+       var timer;
+
+       function showRemaining() {
+           var now = new Date();
+           var distance = end - now;
+           if (distance < 0) {
+               clearInterval(timer);
+               document.getElementById(id).innerHTML = 'EXPIRED!';
+               return;
+           }
+           var days = Math.floor(distance / _day);
+           var hours = Math.floor((distance % _day) / _hour);
+           var minutes = Math.floor((distance % _hour) / _minute);
+           var seconds = Math.floor((distance % _minute) / _second);
+
+           document.getElementById(id).innerHTML = days + 'd ';
+           document.getElementById(id).innerHTML += hours + 'h ';
+           document.getElementById(id).innerHTML += minutes + 'm ';
+           document.getElementById(id).innerHTML += seconds + 's';
+       }
+
+       timer = setInterval(showRemaining, 1000);
 }
